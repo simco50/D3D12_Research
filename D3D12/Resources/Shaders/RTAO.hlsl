fcc75fdfb0cfae525a1ee03fb601ad3a5c9c3c7f
Simon Coenen <sim2.coenen@gmail.com>
2020-04-08 20:46:14 +0100

    Add some comments to TraceRay

--- D3D12/Resources/Shaders/RTAO.hlsl
+++ D3D12/Resources/Shaders/RTAO.hlsl
@@ -79,21 +79,45 @@ void RayGen()
 		TraceRay(
 			//AccelerationStructure
 			SceneBVH,
+
 			//RayFlags
 			RAY_FLAG_CULL_BACK_FACING_TRIANGLES |
         	RAY_FLAG_FORCE_OPAQUE,
+
 			//InstanceInclusionMask
+			// Instance inclusion mask, which can be used to mask out some geometry to this ray by
+  			// and-ing the mask with a geometry mask. The 0xFF flag then indicates no geometry will be
+			// masked
 			0xFF,
+
 			//RayContributionToHitGroupIndex
+			// Depending on the type of ray, a given object can have several hit groups attached
+			// (ie. what to do when hitting to compute regular shading, and what to do when hitting
+			// to compute shadows). Those hit groups are specified sequentially in the SBT, so the value
+			// below indicates which offset (on 4 bits) to apply to the hit groups for this ray.
 			0,
+
 			//MultiplierForGeometryContributionToHitGroupIndex
+			// The offsets in the SBT can be computed from the object ID, its instance ID, but also simply
+			// by the order the objects have been pushed in the acceleration structure. This allows the
+			// application to group shaders in the SBT in the same order as they are added in the AS, in
+			// which case the value below represents the stride (4 bits representing the number of hit
+			// groups) between two consecutive objects.
 			0,
+
 			//MissShaderIndex
+			// Index of the miss shader to use in case several consecutive miss shaders are present in the
+			// SBT. This allows to change the behavior of the program when no geometry have been hit, for
+			// example one to return a sky color for regular rendering, and another returning a full
+			// visibility value for shadow rays.
 			0,
+
 			//Ray
 			ray,
+
 			//Payload
 			payload);
+
 		accumulatedAo += payload.hitDistance != 0;
 	}
 	accumulatedAo /= cSamples;
