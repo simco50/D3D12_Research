46b31c4ce4bb0da1e4bbd4bf30f472b82709e40d
Simon Coenen <sim2.coenen@gmail.com>
2020-07-05 21:36:15 +0100

    Integrate async task queue

---
+++ D3D12/Core/TaskQueue.h
@@ -0,0 +1,38 @@
+#pragma once
+#include <atomic>
+struct AsyncTask;
+
+struct TaskDistributeArgs
+{
+	int JobIndex;
+	int ThreadIndex;
+};
+
+DECLARE_DELEGATE(AsyncTaskDelegate, int);
+DECLARE_DELEGATE(AsyncDistributeDelegate, TaskDistributeArgs);
+
+using TaskContext = std::atomic<uint32>;
+
+class TaskQueue
+{
+public:
+	~TaskQueue();
+
+	static void Initialize(uint32 threads);
+	template<typename Callback>
+	static void Execute(Callback&& action, TaskContext& context)
+	{
+		AddWorkItem(AsyncTaskDelegate::CreateLambda(std::forward<Callback>(action)), context);
+	}
+	template<typename Callback>
+	static void ExecuteMany(Callback&& action, TaskContext& context, uint32 count, int32 groupSize = -1)
+	{
+		Distribute(context, AsyncDistributeDelegate::CreateLambda(std::forward<Callback>(action)), count, groupSize);
+	}
+	static void Join(TaskContext& context);
+
+private:
+	static void Distribute(TaskContext& context, const AsyncDistributeDelegate& action, uint32 count, int32 groupSize = -1);
+	static void AddWorkItem(const AsyncTaskDelegate& action, TaskContext& context);
+	static void CreateThreads(size_t count);
+};
