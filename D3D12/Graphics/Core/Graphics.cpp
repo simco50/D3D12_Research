acb27301da61c9b773a0ecb4f81f0aa895a42604
Simon Coenen <sim2.coenen@gmail.com>
2021-01-20 22:09:06 +0000

    VRS check

--- D3D12/Graphics/Core/Graphics.cpp
+++ D3D12/Graphics/Core/Graphics.cpp
@@ -1311,6 +1311,13 @@ void Graphics::InitD3D()
 			m_ShaderModelMajor = shaderModelSupport.HighestShaderModel >> 0x4;
 			m_ShaderModelMinor = shaderModelSupport.HighestShaderModel & 0xF;
 		}
+
+		D3D12_FEATURE_DATA_D3D12_OPTIONS6 caps6{};
+		if (SUCCEEDED(m_pDevice->CheckFeatureSupport(D3D12_FEATURE_D3D12_OPTIONS6, &caps6, sizeof(D3D12_FEATURE_DATA_D3D12_OPTIONS6))))
+		{
+			m_VRSTier = caps6.VariableShadingRateTier;
+			m_VRSTileSize = caps6.ShadingRateImageTileSize;
+		}
 		D3D12_FEATURE_DATA_D3D12_OPTIONS7 caps7{};
 		if (SUCCEEDED(m_pDevice->CheckFeatureSupport(D3D12_FEATURE_D3D12_OPTIONS7, &caps7, sizeof(D3D12_FEATURE_DATA_D3D12_OPTIONS7))))
 		{
