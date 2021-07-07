d7008a6b6b75b318d34a143634086afe9d65b21e
Simon Coenen <sim2.coenen@gmail.com>
2021-07-07 13:50:02 +0100

    Replace tinygltf with cgltf

--- D3D12/Graphics/Mesh.cpp
+++ D3D12/Graphics/Mesh.cpp
@@ -7,18 +7,10 @@
 #include "Core/Paths.h"
 #include "Content/Image.h"
 
-#include "External/Stb/stb_image.h"
-#include "External/Stb/stb_image_write.h"
-#include "External/json/json.hpp"
-
 #pragma warning(push)
-#pragma warning(disable: 4702) //unreachable code
-#define TINYGLTF_NO_INCLUDE_JSON
-#define TINYGLTF_NO_EXTERNAL_IMAGE 
-#define TINYGLTF_NO_INCLUDE_STB_IMAGE
-#define TINYGLTF_NO_INCLUDE_STB_IMAGE_WRITE
-#define TINYGLTF_IMPLEMENTATION
-#include "External/tinygltf/tiny_gltf.h"
+#pragma warning(disable: 4996) //_CRT_SECURE_NO_WARNINGS
+#define CGLTF_IMPLEMENTATION
+#include "External/cgltf/cgltf.h"
 #pragma warning(pop)
 
 struct Vertex
@@ -39,171 +31,85 @@ Mesh::~Mesh()
 	}
 }
 
-namespace GltfCallbacks
-{
-	bool FileExists(const std::string& abs_filename, void*)
-	{
-		return Paths::FileExists(abs_filename);
-	}
-
-	std::string ExpandFilePath(const std::string& filepath, void*)
-	{
-		DWORD len = ExpandEnvironmentStringsA(filepath.c_str(), NULL, 0);
-		char* str = new char[len];
-		ExpandEnvironmentStringsA(filepath.c_str(), str, len);
-		std::string s(str);
-		delete[] str;
-		return s;
-	}
-
-	bool ReadWholeFile(std::vector<unsigned char>* out, std::string* err, const std::string& filePath, void*)
-	{
-		std::ifstream s(filePath, std::ios::binary | std::ios::ate);
-		if (s.fail())
-		{
-			return false;
-		}
-		out->resize((size_t)s.tellg());
-		s.seekg(0);
-		s.read((char*)out->data(), out->size());
-		return true;
-	}
-
-	bool WriteWholeFile(std::string* err, const std::string& filePath, const std::vector<unsigned char>& contents, void*)
-	{
-		std::ofstream s(filePath, std::ios::binary);
-		if (s.fail())
-		{
-			return false;
-		}
-		s.write((char*)contents.data(), contents.size());
-		return true;
-	}
-}
-
 bool Mesh::Load(const char* pFilePath, GraphicsDevice* pDevice, CommandContext* pContext, float uniformScale /*= 1.0f*/)
 {
-	tinygltf::TinyGLTF loader;
-	std::string err, warn;
-
-	tinygltf::FsCallbacks callbacks;
-	callbacks.ReadWholeFile = GltfCallbacks::ReadWholeFile;
-	callbacks.WriteWholeFile = GltfCallbacks::WriteWholeFile;
-	callbacks.FileExists = GltfCallbacks::FileExists;
-	callbacks.ExpandFilePath = GltfCallbacks::ExpandFilePath;
-	loader.SetFsCallbacks(callbacks);
-
-	std::string extension = Paths::GetFileExtenstion(pFilePath);
-	tinygltf::Model model;
-	bool ret = extension == "gltf" ? loader.LoadASCIIFromFile(&model, &err, &warn, pFilePath) : loader.LoadBinaryFromFile(&model, &err, &warn, pFilePath);
-	if (!ret)
+	cgltf_options options{};
+	cgltf_data* pGltfData = nullptr;
+	cgltf_result result = cgltf_parse_file(&options, pFilePath, &pGltfData);
+	if (result != cgltf_result_success)
 	{
-		E_LOG(Warning, "GLTF - Failed to load '%s': '%s'", pFilePath, err.c_str());
+		E_LOG(Warning, "GLTF - Failed to load '%s'", pFilePath);
 		return false;
 	}
-	if (warn.length() > 0)
+	result = cgltf_load_buffers(&options, pGltfData, pFilePath);
+	if (result != cgltf_result_success)
 	{
-		E_LOG(Warning, "GLTF - Load warning: %s", warn.c_str());
+		E_LOG(Warning, "GLTF - Failed to load buffers '%s'", pFilePath);
+		return false;
 	}
 
 	// Load unique textures;
 	std::map<StringHash, Texture*> textureMap;
+	std::map<const cgltf_material*, int> materialMap;
 
-	m_Materials.reserve(model.materials.size());
-	for (const tinygltf::Material& gltfMaterial : model.materials)
+	m_Materials.reserve(pGltfData->materials_count);
+	for (size_t i = 0; i < pGltfData->materials_count; ++i)
 	{
+		const cgltf_material& gltfMaterial = pGltfData->materials[i];
+		materialMap[&gltfMaterial] = (int)m_Materials.size();
+
 		m_Materials.push_back(Material());
 		Material& material = m_Materials.back();
 
-		auto baseColorTexture = gltfMaterial.values.find("baseColorTexture");
-		auto metallicRoughnessTexture = gltfMaterial.values.find("metallicRoughnessTexture");
-		auto baseColorFactor = gltfMaterial.values.find("baseColorFactor");
-		auto roughnessFactor = gltfMaterial.values.find("roughnessFactor");
-		auto metallicFactor = gltfMaterial.values.find("metallicFactor");
-		auto normalTexture = gltfMaterial.additionalValues.find("normalTexture");
-		auto emissiveTexture = gltfMaterial.additionalValues.find("emissiveTexture");
-		auto emissiveFactor = gltfMaterial.additionalValues.find("emissiveFactor");
-		auto alphaCutoff = gltfMaterial.additionalValues.find("alphaCutoff");
-		auto alphaMode = gltfMaterial.additionalValues.find("alphaMode");
-
-		auto RetrieveTexture = [this, &textureMap, &model, pDevice, pContext, pFilePath](bool isValid, auto gltfParameter, Texture** pTarget, bool srgb)
+		auto RetrieveTexture = [this, &textureMap, pDevice, pContext, pFilePath](const cgltf_texture_view texture, bool srgb) -> Texture*
 		{
-			if (isValid)
+			if (texture.texture)
 			{
-				int index = gltfParameter->second.TextureIndex();
-				if (index < model.textures.size())
+				const cgltf_image* pImage = texture.texture->image;
+				StringHash pathHash = StringHash(pImage->uri);
+				pathHash.Combine((int)srgb);
+				auto it = textureMap.find(pathHash);
+				std::unique_ptr<Texture> pTex = std::make_unique<Texture>(pDevice, pImage->uri);
+				if (it == textureMap.end())
 				{
-					const tinygltf::Texture& texture = model.textures[index];
-					const tinygltf::Image& image = model.images[texture.source];
-					StringHash pathHash = StringHash(image.uri.c_str());
-					pathHash.Combine((int)srgb);
-					auto it = textureMap.find(pathHash);
-					std::unique_ptr<Texture> pTex = std::make_unique<Texture>(pDevice, image.uri.c_str());
-					if (it == textureMap.end())
+					bool success = pTex->Create(pContext, Paths::Combine(Paths::GetDirectoryPath(pFilePath), pImage->uri).c_str(), srgb);
+					if (success)
 					{
-						bool success = false;
-						if (image.uri.empty())
-						{
-							Image img;
-							img.SetSize(image.width, image.height, 4);
-							if (img.SetData(image.image.data()))
-							{
-								success = pTex->Create(pContext, img, srgb);
-							}
-						}
-						else
-						{
-							success = pTex->Create(pContext, Paths::Combine(Paths::GetDirectoryPath(pFilePath), image.uri).c_str(), srgb);
-						}
-						if (success)
-						{
-							m_Textures.push_back(std::move(pTex));
-							textureMap[pathHash] = m_Textures.back().get();
-							*pTarget = m_Textures.back().get();
-						}
+						m_Textures.push_back(std::move(pTex));
+						textureMap[pathHash] = m_Textures.back().get();
+						return m_Textures.back().get();
 					}
 					else
 					{
-						*pTarget = it->second;
+						E_LOG(Warning, "GLTF - Failed to load texture '%s' for '%s'", pImage->uri, pFilePath);
 					}
 				}
+				else
+				{
+					return it->second;
+				}
 			}
+			return nullptr;
 		};
 
-		RetrieveTexture(baseColorTexture != gltfMaterial.values.end(), baseColorTexture, &material.pDiffuseTexture, true);
-		RetrieveTexture(normalTexture != gltfMaterial.additionalValues.end(), normalTexture, &material.pNormalTexture, false);
-		RetrieveTexture(emissiveTexture != gltfMaterial.additionalValues.end(), emissiveTexture, &material.pEmissiveTexture, true);
-		RetrieveTexture(metallicRoughnessTexture != gltfMaterial.values.end(), metallicRoughnessTexture, &material.pRoughnessMetalnessTexture, false);
-
-		if (baseColorFactor != gltfMaterial.values.end())
+		if (gltfMaterial.has_pbr_metallic_roughness)
 		{
-			material.BaseColorFactor.x = (float)baseColorFactor->second.ColorFactor()[0];
-			material.BaseColorFactor.y = (float)baseColorFactor->second.ColorFactor()[1];
-			material.BaseColorFactor.z = (float)baseColorFactor->second.ColorFactor()[2];
-			material.BaseColorFactor.w = (float)baseColorFactor->second.ColorFactor()[3];
+			material.pDiffuseTexture = RetrieveTexture(gltfMaterial.pbr_metallic_roughness.base_color_texture, true);
+			material.pRoughnessMetalnessTexture = RetrieveTexture(gltfMaterial.pbr_metallic_roughness.metallic_roughness_texture, false);
+			material.BaseColorFactor.x = gltfMaterial.pbr_metallic_roughness.base_color_factor[0];
+			material.BaseColorFactor.y = gltfMaterial.pbr_metallic_roughness.base_color_factor[1];
+			material.BaseColorFactor.z = gltfMaterial.pbr_metallic_roughness.base_color_factor[2];
+			material.BaseColorFactor.w = gltfMaterial.pbr_metallic_roughness.base_color_factor[3];
+			material.MetalnessFactor = gltfMaterial.pbr_metallic_roughness.metallic_factor;
+			material.RoughnessFactor = gltfMaterial.pbr_metallic_roughness.roughness_factor;
 		}
-		if (emissiveFactor != gltfMaterial.additionalValues.end())
-		{
-			material.EmissiveFactor.x = (float)emissiveFactor->second.ColorFactor()[0];
-			material.EmissiveFactor.y = (float)emissiveFactor->second.ColorFactor()[1];
-			material.EmissiveFactor.z = (float)emissiveFactor->second.ColorFactor()[2];
-		}
-		if (roughnessFactor != gltfMaterial.values.end())
-		{
-			material.RoughnessFactor = (float)roughnessFactor->second.Factor();
-		}
-		if (metallicFactor != gltfMaterial.values.end())
-		{
-			material.MetalnessFactor = (float)metallicFactor->second.Factor();
-		}
-		if (alphaMode != gltfMaterial.additionalValues.end())
-		{
-			material.IsTransparent = alphaMode->second.string_value != "OPAQUE";
-		}
-		if (alphaCutoff != gltfMaterial.additionalValues.end())
-		{
-			material.AlphaCutoff = (float)alphaMode->second.Factor();
-		}
+		material.AlphaCutoff = gltfMaterial.alpha_cutoff;
+		material.IsTransparent = gltfMaterial.alpha_mode != cgltf_alpha_mode_opaque;
+		material.pEmissiveTexture = RetrieveTexture(gltfMaterial.emissive_texture, true);
+		material.EmissiveFactor.x = gltfMaterial.emissive_factor[0];
+		material.EmissiveFactor.y = gltfMaterial.emissive_factor[1];
+		material.EmissiveFactor.z = gltfMaterial.emissive_factor[2];
+		material.pNormalTexture = RetrieveTexture(gltfMaterial.normal_texture, false);
 	}
 
 	std::vector<uint32> indices;
@@ -217,33 +123,32 @@ bool Mesh::Load(const char* pFilePath, GraphicsDevice* pDevice, CommandContext* 
 		uint32 MaterialIndex = 0;
 	};
 	std::vector<MeshData> meshDatas;
-	std::vector<std::vector<int>> meshToPrimitives;
+	std::map<const cgltf_mesh*, std::vector<int>> meshToPrimitives;
 	int primitiveIndex = 0;
 
-	for (const tinygltf::Mesh& mesh : model.meshes)
+	for (size_t meshIdx = 0; meshIdx < pGltfData->meshes_count; ++meshIdx)
 	{
+		const cgltf_mesh& mesh = pGltfData->meshes[meshIdx];
 		std::vector<int> primtives;
-		for (const tinygltf::Primitive& primitive : mesh.primitives)
+		for (size_t primIdx = 0; primIdx < mesh.primitives_count; ++primIdx)
 		{
+			const cgltf_primitive& primitive = mesh.primitives[primIdx];
 			primtives.push_back(primitiveIndex++);
 			MeshData meshData;
 			uint32 vertexOffset = (uint32)vertices.size();
 			meshData.VertexOffset = vertexOffset;
 
-			// Indices
-			const tinygltf::Accessor& indexAccessor = model.accessors[primitive.indices];
-			const tinygltf::BufferView& indexBufferView = model.bufferViews[indexAccessor.bufferView];
-			const tinygltf::Buffer& indexBuffer = model.buffers[indexBufferView.buffer];
-
-			int indexStride = indexAccessor.ByteStride(indexBufferView);
-			size_t indexCount = indexAccessor.count;
+			size_t indexStride = primitive.indices->stride;
+			size_t indexCount = primitive.indices->count;
 			uint32 indexOffset = (uint32)indices.size();
 			indices.resize(indices.size() + indexCount);
 
 			meshData.IndexOffset = indexOffset;
 			meshData.NumIndices = (uint32)indexCount;
+			meshData.MaterialIndex = materialMap[primitive.material];
 
-			const unsigned char* indexData = indexBuffer.data.data() + indexAccessor.byteOffset + indexBufferView.byteOffset;
+			const cgltf_buffer_view* pIndexView = primitive.indices->buffer_view;
+			const char* indexData = (char*)pIndexView->buffer->data + pIndexView->offset + primitive.indices->offset;
 
 			if (indexStride == 4)
 			{
@@ -264,71 +169,67 @@ bool Mesh::Load(const char* pFilePath, GraphicsDevice* pDevice, CommandContext* 
 				noEntry();
 			}
 
-			meshData.MaterialIndex = primitive.material;
-
-			for (const auto& attribute : primitive.attributes)
+			for (size_t attrIdx = 0; attrIdx < primitive.attributes_count; ++attrIdx)
 			{
-				const std::string name = attribute.first;
-				int index = attribute.second;
+				const cgltf_attribute& attribute = primitive.attributes[attrIdx];
+				const char* pName = attribute.name;
 
-				const tinygltf::Accessor& accessor = model.accessors[index];
-				const tinygltf::BufferView& bufferView = model.bufferViews[accessor.bufferView];
-				const tinygltf::Buffer& buffer = model.buffers[bufferView.buffer];
-				int stride = accessor.ByteStride(bufferView);
-				const unsigned char* pData = buffer.data.data() + accessor.byteOffset + bufferView.byteOffset;
+				const cgltf_buffer_view* pAttrView = attribute.data->buffer_view;
+				size_t stride = attribute.data->stride;
+				const char* pData = (char*)pAttrView->buffer->data + pAttrView->offset + attribute.data->offset;
 
 				if (meshData.NumVertices == 0)
 				{
-					vertices.resize(vertices.size() + accessor.count);
-					meshData.NumVertices = (uint32)accessor.count;
+					vertices.resize(vertices.size() + attribute.data->count);
+					meshData.NumVertices = (uint32)attribute.data->count;
 				}
 
-				if (name == "POSITION")
+				if (strcmp(pName, "POSITION") == 0)
 				{
 					check(stride == sizeof(Vector3));
 					const Vector3* pPositions = (Vector3*)pData;
-					for (size_t i = 0; i < accessor.count; ++i)
+					for (size_t i = 0; i < attribute.data->count; ++i)
 					{
 						vertices[i + vertexOffset].Position = pPositions[i];
 					}
 				}
-				else if (name == "NORMAL")
+				else if (strcmp(pName, "NORMAL") == 0)
 				{
 					check(stride == sizeof(Vector3));
 					const Vector3* pNormals = (Vector3*)pData;
-					for (size_t i = 0; i < accessor.count; ++i)
+					for (size_t i = 0; i < attribute.data->count; ++i)
 					{
 						vertices[i + vertexOffset].Normal = pNormals[i];
 					}
 				}
-				else if (name == "TANGENT")
+				else if (strcmp(pName, "TANGENT") == 0)
 				{
 					check(stride == sizeof(Vector4));
 					const Vector4* pTangents = (Vector4*)pData;
-					for (size_t i = 0; i < accessor.count; ++i)
+					for (size_t i = 0; i < attribute.data->count; ++i)
 					{
 						vertices[i + vertexOffset].Tangent = Vector3(pTangents[i]);
 						float sign = pTangents[i].w;
 						vertices[i + vertexOffset].Bitangent = Vector3(sign, sign, sign);
 					}
 				}
-				else if (name == "TEXCOORD_0")
+				else if (strcmp(pName, "TEXCOORD_0") == 0)
 				{
 					check(stride == sizeof(Vector2));
 					Vector2* pTexCoords = (Vector2*)pData;
-					for (size_t i = 0; i < accessor.count; ++i)
+					for (size_t i = 0; i < attribute.data->count; ++i)
 					{
 						vertices[i + vertexOffset].TexCoord = pTexCoords[i];
 					}
 				}
 				else
 				{
-					E_LOG(Warning, "GLTF - Attribute '%s' is unsupported", name.c_str());
+					E_LOG(Warning, "GLTF - Attribute '%s' is unsupported", pName);
 				}
 			}
 			meshDatas.push_back(meshData);
 		}
-		meshToPrimitives.push_back(primtives);
+		meshToPrimitives[&mesh] = primtives;
 	}
 
 	// Generate bitangents.
@@ -352,56 +253,66 @@ bool Mesh::Load(const char* pFilePath, GraphicsDevice* pDevice, CommandContext* 
 	m_pGeometryData = pDevice->CreateBuffer(BufferDesc::CreateBuffer(bufferSize, BufferFlag::ShaderResource | BufferFlag::ByteAddress), "Geometry Buffer");
 	pContext->InsertResourceBarrier(m_pGeometryData.get(), D3D12_RESOURCE_STATE_COPY_DEST);
 
-	const tinygltf::Scene& gltfScene = model.scenes[Math::Max(0, model.defaultScene)];
-	for (size_t i = 0; i < gltfScene.nodes.size(); i++)
+		
+	const cgltf_scene* pGltfScene = pGltfData->scene;
+	for (size_t i = 0; i < pGltfScene->nodes_count; i++)
 	{
-		int nodeIdx = gltfScene.nodes[i];
+		const cgltf_node* pParent = pGltfScene->nodes[i];
 		struct QueuedNode
 		{
-			int Index;
+			const cgltf_node* pNode;
 			Matrix Transform;
 		};
 		std::queue<QueuedNode> toProcess;
 		QueuedNode rootNode;
-		rootNode.Index = nodeIdx;
+		rootNode.pNode = pParent;
 		rootNode.Transform = Matrix::CreateScale(uniformScale);
 		toProcess.push(rootNode);
 		while (!toProcess.empty())
 		{
 			QueuedNode currentNode = toProcess.front();
 			toProcess.pop();
-			const tinygltf::Node& node = model.nodes[currentNode.Index];
-			Vector3 scale = node.scale.size() == 0 ? Vector3::One : Vector3((float)node.scale[0], (float)node.scale[1], (float)node.scale[2]);
-			Quaternion rotation = node.rotation.size() == 0 ? Quaternion::Identity : Quaternion((float)node.rotation[0], (float)node.rotation[1], (float)node.rotation[2], (float)node.rotation[3]);
-			Vector3 translation = node.translation.size() == 0 ? Vector3::Zero : Vector3((float)node.translation[0], (float)node.translation[1], (float)node.translation[2]);
-
-			Matrix matrix = node.matrix.size() == 0 ? Matrix::Identity : Matrix(
-				(float)node.matrix[0], (float)node.matrix[1], (float)node.matrix[2], (float)node.matrix[3],
-				(float)node.matrix[4], (float)node.matrix[5], (float)node.matrix[6], (float)node.matrix[7],
-				(float)node.matrix[8], (float)node.matrix[9], (float)node.matrix[10], (float)node.matrix[11],
-				(float)node.matrix[12], (float)node.matrix[13], (float)node.matrix[14], (float)node.matrix[15]
-			);
+			const cgltf_node* pNode = currentNode.pNode;
+			Matrix transform = currentNode.Transform;
+			if (pNode->has_matrix)
+			{
+				transform *= Matrix(pNode->matrix);
+			}
+			if (pNode->has_scale)
+			{
+				transform *= Matrix::CreateScale(Vector3(pNode->scale));
+			}
+			if (pNode->has_rotation)
+			{
+				transform *= Matrix::CreateFromQuaternion(Quaternion(pNode->rotation));
+			}
+			if (pNode->has_translation)
+			{
+				transform *= Matrix::CreateTranslation(Vector3(pNode->translation));
+			}
 
 			SubMeshInstance newNode;
-			newNode.Transform = currentNode.Transform * matrix * Matrix::CreateScale(scale) * Matrix::CreateFromQuaternion(rotation) * Matrix::CreateTranslation(translation);
-			if (node.mesh >= 0)
+			newNode.Transform = transform;
+			if (pNode->mesh)
 			{
-				for (int primitive : meshToPrimitives[node.mesh])
+				for (int primitive : meshToPrimitives[pNode->mesh])
 				{
 					newNode.MeshIndex = primitive;
 					m_MeshInstances.push_back(newNode);
 				}
 			}
 
-			for (int child : node.children)
+			for (size_t childIdx = 0; childIdx < pNode->children_count; ++childIdx)
 			{
 				QueuedNode q;
-				q.Index = child;
+				q.pNode = pNode->children[childIdx];
 				q.Transform = newNode.Transform;
 				toProcess.push(q);
 			}
 		}
 	}
+
+	cgltf_free(pGltfData);
 
 	uint64 dataOffset = 0;
 	auto CopyData = [this, &dataOffset, &pContext](void* pSource, uint64 size)
