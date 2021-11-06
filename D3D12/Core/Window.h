83ce58499d391056dd33c7970f99d4b923ecb18c
Simon Coenen <sim2.coenen@gmail.com>
2021-11-06 16:00:16 +0000

    Seperate window file

---
+++ D3D12/Core/Window.h
@@ -0,0 +1,52 @@
+#pragma once
+
+class Window
+{
+public:
+	static constexpr const char* WINDOW_CLASS_NAME = "WndClass";
+
+	Window(const char* pTitle, uint32 width, uint32 height);
+
+	~Window();
+
+	bool PollMessages();
+
+	void SetWindowTitle(const char* pTitle);
+
+	HWND GetNativeWindow() const { return m_Window; }
+	IntVector2 GetRect() const { return IntVector2(m_DisplayWidth, m_DisplayHeight); }
+
+	DECLARE_MULTICAST_DELEGATE(OnFocusChangedDelegate, bool);
+	OnFocusChangedDelegate OnFocusChanged;
+
+	DECLARE_MULTICAST_DELEGATE(OnResizeDelegate, uint32, uint32);
+	OnResizeDelegate OnResize;
+
+	DECLARE_MULTICAST_DELEGATE(OnCharInputDelegate, uint32);
+	OnCharInputDelegate OnCharInput;
+
+	DECLARE_MULTICAST_DELEGATE(OnKeyInputDelegate, uint32, bool);
+	OnKeyInputDelegate OnKeyInput;
+
+	DECLARE_MULTICAST_DELEGATE(OnMouseInputDelegate, uint32, bool);
+	OnMouseInputDelegate OnMouseInput;
+
+	DECLARE_MULTICAST_DELEGATE(OnMouseMoveDelegate, uint32, uint32);
+	OnMouseMoveDelegate OnMouseMove;
+
+	DECLARE_MULTICAST_DELEGATE(OnMouseScrollDelegate, float);
+	OnMouseScrollDelegate OnMouseScroll;
+
+private:
+	static LRESULT CALLBACK WndProcStatic(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+
+	LRESULT WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+
+private:
+	HWND m_Window = nullptr;
+	bool m_Minimized = false;
+	bool m_Maximized = false;
+	int m_DisplayWidth = 0;
+	int m_DisplayHeight = 0;
+	bool m_IsResizing = false;
+};
