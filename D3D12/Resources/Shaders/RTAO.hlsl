f1c490a9908892c567deb9699f36ef4350dd69ab
Simon Coenen <sim2.coenen@gmail.com>
2022-03-23 21:11:36 +0000

    Fix RTAO shader

--- D3D12/Resources/Shaders/RTAO.hlsl
+++ D3D12/Resources/Shaders/RTAO.hlsl
@@ -14,37 +14,23 @@ struct PassData
 
 ConstantBuffer<PassData> cPass : register(b0);
 
-// Utility function to get a vector perpendicular to an input vector
-// From Michael M. Stark - https://blog.selfshadow.com/2011/10/17/perp-vectors/
-float3 GetPerpendicularVector(float3 u)
+float3x3 TangentMatrix(float3 z)
 {
-	float3 a = abs(u);
-	uint uyx = sign(a.x - a.y);
-	uint uzx = sign(a.x - a.z);
-	uint uzy = sign(a.y - a.z);
-
-	uint xm = uyx & uzx;
-	uint ym = (1^xm) & uzy;
-	uint zm = 1^(xm & ym);
-
-	float3 v = cross(u, float3(xm, ym, zm));
-	return v;
+    float3 ref = abs(dot(z, float3(0, 1, 0))) > 0.99f ? float3(0, 0, 1) : float3(0, 1, 0);
+    float3 x = normalize(cross(ref, z));
+    float3 y = cross(z, x);
+    return float3x3(x, y, z);
 }
 
-// Get a cosine-weighted random vector centered around a specified normal direction.
-float3 GetCosHemisphereSample(inout uint randSeed, float3 hitNorm)
+float3 RandomCosineWeightedRay(float3 n, inout uint seed)
 {
-	// Get 2 random numbers to select our sample with
-	float2 randVal = float2(Random01(randSeed), Random01(randSeed));
-
-	// Cosine weighted hemisphere sample from RNG
-	float3 bitangent = GetPerpendicularVector(hitNorm);
-	float3 tangent = cross(bitangent, hitNorm);
-	float r = sqrt(randVal.x);
-	float phi = 2.0f * 3.14159265f * randVal.y;
-
-	// Get our cosine-weighted hemisphere lobe sample direction
-	return tangent * (r * cos(phi)) + bitangent * (r * sin(phi)) + hitNorm.xyz * sqrt(1 - randVal.x);
+	float2 r = float2(Random01(seed), Random01(seed));
+    float2 rand_sample = max(0.00001f, r);
+    float phi = 2.0f * PI * rand_sample.y;
+    float cos_theta = sqrt(rand_sample.x);
+    float sin_theta = sqrt(1 - rand_sample.x);
+    float3 t = float3(sin_theta * cos(phi), sin_theta * sin(phi), cos_theta);
+    return normalize(mul(t, TangentMatrix(n)));
 }
 
 [shader("raygeneration")]
@@ -54,17 +40,18 @@ void RayGen()
 	float2 dimInv = rcp((float2)launchDim.xy);
 	uint2 launchIndex = DispatchRaysIndex().xy;
 	uint launchIndex1d = launchIndex.x + launchIndex.y * launchDim.x;
-	float2 uv = (float2)launchIndex * dimInv;
+	float2 uv = (launchIndex + 0.5f) * dimInv;
 
 	float3 world = WorldFromDepth(uv, tSceneDepth.SampleLevel(sLinearClamp, uv, 0).r, cView.ViewProjectionInverse);
 	float3 normal = NormalFromDepth(tSceneDepth, sLinearClamp, uv, dimInv, cView.ProjectionInverse);
+	normal = mul(normal, (float3x3)cView.ViewInverse);
 
 	uint randSeed = SeedThread(launchIndex, launchDim, cView.FrameIndex);
 
 	float accumulatedAo = 0.0f;
 	for(int i = 0; i < cPass.Samples; ++i)
 	{
-		float3 randomDirection = GetCosHemisphereSample(randSeed, normal.xyz);
+		float3 randomDirection = RandomCosineWeightedRay(normal.xyz, randSeed);
 		float hit = !TraceOcclusionRay(world, randomDirection, cPass.Radius);
 		accumulatedAo += hit;
 	}
