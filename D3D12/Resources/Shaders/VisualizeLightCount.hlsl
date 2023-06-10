4043cdcc8e453609beb5b58321f95e0507588f24
Simon Coenen <sim2.coenen@gmail.com>
2023-06-10 12:20:26 +0200

    Fix lightgrid format in visualization shader

--- D3D12/Resources/Shaders/VisualizeLightCount.hlsl
+++ D3D12/Resources/Shaders/VisualizeLightCount.hlsl
@@ -1,5 +1,6 @@
 #include "Common.hlsli"
 #include "ColorMaps.hlsli"
+#include "ShaderDebugRender.hlsli"
 
 #define BLOCK_SIZE 16
 
@@ -18,16 +19,18 @@ RWTexture2D<float4> uOutput : register(u0);
 #if TILED_FORWARD
 Texture2D<uint2> tLightGrid : register(t2);
 #elif CLUSTERED_FORWARD
-StructuredBuffer<uint2> tLightGrid : register(t2);
+StructuredBuffer<uint> tLightGrid : register(t2);
 #endif
 
-static int2 SobelWeights[] = {
+static const int MaxNumLights = 10;
+
+static const int2 SobelWeights[] = {
 	int2(1,  1), int2(0,  2), int2(-1,  1),
 	int2(2,  0), int2(0,  0), int2(-2,  0),
 	int2(1, -1), int2(0, -2), int2(-1, -1),
 };
 
-static int2 SobelOffsets[] = {
+static const int2 SobelOffsets[] = {
 	int2(-1,  1), int2(0,  1), int2(1,  1),
 	int2(-1,  0), int2(0,  0), int2(1,  0),
 	int2(-1, -1), int2(0, -1), int2(1, -1),
@@ -48,7 +51,7 @@ float3 ApplyEdgeDetection(uint2 pixel, float3 color)
 
 float3 GetColor(uint2 pixel, uint lightCount)
 {
-	float3 color = Inferno(saturate(0.1f *  lightCount));
+	float3 color = Inferno(saturate((float)lightCount / MaxNumLights));
 	return ApplyEdgeDetection(pixel, color);
 }
 
@@ -67,8 +70,25 @@ void DebugLightDensityCS(uint3 threadId : SV_DispatchThreadID)
 	uint slice = floor(log(viewDepth) * cPass.LightGridParams.x - cPass.LightGridParams.y);
 	uint3 clusterIndex3D = uint3(floor(threadId.xy / cPass.ClusterSize), slice);
 	uint clusterIndex1D = clusterIndex3D.x + (cPass.ClusterDimensions.x * (clusterIndex3D.y + cPass.ClusterDimensions.y * clusterIndex3D.z));
-	uint lightCount = tLightGrid[clusterIndex1D].y;
+	uint lightCount = tLightGrid[clusterIndex1D * 2 + 1];
 #endif
 	uOutput[threadId.xy] = float4(GetColor(threadId.xy, lightCount), 1);
 
+	// Draw legend
+	const float boxSize = 40;
+	const float2 topLeft = cView.ViewportDimensions - float2(boxSize + 10, boxSize * MaxNumLights + 10);
+
+	if(threadId.x < MaxNumLights && threadId.y == 0)
+	{
+		float2 cursor = float2(topLeft.x, topLeft.y + threadId.x * boxSize);
+		DrawRect(topLeft * cView.ViewportDimensionsInv, (cursor + boxSize) * cView.ViewportDimensionsInv, RectMode::MinMax);
+		TextWriter writer = CreateTextWriter(cursor + 0.2f * boxSize);
+		writer.Int(threadId.x);
+	}
+
+	float2 boxPos = ((int2)threadId.xy - topLeft) / float2(boxSize, boxSize * MaxNumLights);
+	if(all(boxPos >= 0) && all(boxPos <= 1))
+	{
+		uOutput[threadId.xy] = float4(Inferno(boxPos.y), 1);
+	}
 }
