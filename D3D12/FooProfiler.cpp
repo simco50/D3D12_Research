2a826147bcd6c7da148c726c81dcd2837728d86d
Simon Coenen <sim2.coenen@gmail.com>
2023-08-06 19:19:15 +0200

    Some renames

--- D3D12/FooProfiler.cpp
+++ D3D12/FooProfiler.cpp
@@ -8,18 +8,6 @@
 FooProfiler gProfiler;
 GPUProfiler gGPUProfiler;
 
-struct HUDContext
-{
-	float TimelineScale = 5.0f;
-	ImVec2 TimelineOffset = ImVec2(0.0f, 0.0f);
-
-	bool IsSelectingRange = false;
-	float RangeSelectionStart = 0.0f;
-	char SearchString[128]{};
-};
-
-static HUDContext gHUDContext;
-
 struct StyleOptions
 {
 	int MaxDepth = 10;
@@ -36,20 +24,36 @@ struct StyleOptions
 	bool DebugMode = false;
 };
 
-static StyleOptions gStyle;
+struct HUDContext
+{
+	StyleOptions Style;
 
-static void EditStyle()
+	float TimelineScale = 5.0f;
+	ImVec2 TimelineOffset = ImVec2(0.0f, 0.0f);
+
+	bool IsSelectingRange = false;
+	float RangeSelectionStart = 0.0f;
+	char SearchString[128]{};
+};
+
+static HUDContext gHUDContext;
+static HUDContext& Context()
+{
+	return gHUDContext;
+}
+
+static void EditStyle(StyleOptions& style)
 {
 	ImGui::PushItemWidth(ImGui::GetContentRegionAvail().x * 0.7f);
-	ImGui::SliderInt("Depth", &gStyle.MaxDepth, 1, 12);
-	ImGui::SliderInt("Max Time", &gStyle.MaxTime, 8, 66);
-	ImGui::SliderFloat("Bar Height", &gStyle.BarHeight, 8, 33);
-	ImGui::SliderFloat("Bar Padding", &gStyle.BarPadding, 0, 5);
-	ImGui::ColorEdit4("Bar Color Multiplier", &gStyle.BarColorMultiplier.x);
-	ImGui::ColorEdit4("Background Text Color", &gStyle.BGTextColor.x);
-	ImGui::ColorEdit4("Foreground Text Color", &gStyle.FGTextColor.x);
-	ImGui::ColorEdit4("Bar Highlight Color", &gStyle.BarHighlightColor.x);
-	ImGui::Checkbox("Debug Mode", &gStyle.DebugMode);
+	ImGui::SliderInt("Depth", &style.MaxDepth, 1, 12);
+	ImGui::SliderInt("Max Time", &style.MaxTime, 8, 66);
+	ImGui::SliderFloat("Bar Height", &style.BarHeight, 8, 33);
+	ImGui::SliderFloat("Bar Padding", &style.BarPadding, 0, 5);
+	ImGui::ColorEdit4("Bar Color Multiplier", &style.BarColorMultiplier.x);
+	ImGui::ColorEdit4("Background Text Color", &style.BGTextColor.x);
+	ImGui::ColorEdit4("Foreground Text Color", &style.FGTextColor.x);
+	ImGui::ColorEdit4("Bar Highlight Color", &style.BarHighlightColor.x);
+	ImGui::Checkbox("Debug Mode", &style.DebugMode);
 	ImGui::PopItemWidth();
 }
 
@@ -92,6 +96,9 @@ static ImColor ColorFromString(const char* pName)
 
 void DrawProfilerHUD()
 {
+	HUDContext& context = Context();
+	StyleOptions& style = context.Style;
+
 	// How many ticks per ms
 	uint64 frequency = 0;
 	QueryPerformanceFrequency((LARGE_INTEGER*)&frequency);
@@ -101,7 +108,7 @@ void DrawProfilerHUD()
 	auto MsToTicks = [&](float ms) { return ms * ticksPerMs; };
 
 	// How many ticks are in the timeline
-	float ticksInTimeline = ticksPerMs * gStyle.MaxTime;
+	float ticksInTimeline = ticksPerMs * style.MaxTime;
 
 	const FooProfiler::SampleHistory& data = gProfiler.GetHistory();
 	const FooProfiler::SampleRegion& frameSample = data.Regions[0];
@@ -116,17 +123,17 @@ void DrawProfilerHUD()
 	ImGui::Text("Search");
 	ImGui::SetNextItemWidth(150);
 	ImGui::SameLine();
-	ImGui::InputText("##Search", gHUDContext.SearchString, ARRAYSIZE(gHUDContext.SearchString));
+	ImGui::InputText("##Search", context.SearchString, ARRAYSIZE(context.SearchString));
 	ImGui::SameLine();
 	if (ImGui::Button(ICON_FA_TIMES "##clearfilter"))
-		gHUDContext.SearchString[0] = 0;
+		context.SearchString[0] = 0;
 	ImGui::SameLine();
 	if (ImGui::Button(ICON_FA_PAINT_BRUSH "##styleeditor"))
 		ImGui::OpenPopup("Style Editor");
 
 	if (ImGui::BeginPopup("Style Editor"))
 	{
-		EditStyle();
+		EditStyle(style);
 		ImGui::EndPopup();
 	}
 
@@ -140,9 +147,9 @@ void DrawProfilerHUD()
 	ImGui::ItemSize(timelineRect);
 
 	// The current (scaled) size of the timeline
-	float timelineWidth = timelineRect.GetWidth() * gHUDContext.TimelineScale;
+	float timelineWidth = timelineRect.GetWidth() * context.TimelineScale;
 
-	ImVec2 cursor = timelineRect.Min + gHUDContext.TimelineOffset;
+	ImVec2 cursor = timelineRect.Min + context.TimelineOffset;
 	ImVec2 cursorStart = cursor;
 	ImDrawList* pDraw = ImGui::GetWindowDrawList();
 
@@ -161,23 +168,23 @@ void DrawProfilerHUD()
 			|	|	|	|
 			|	|	|	|
 		*/
-		pDraw->AddRectFilled(timelineRect.Min, ImVec2(timelineRect.Max.x, timelineRect.Min.y + gStyle.BarHeight), ImColor(0.0f, 0.0f, 0.0f, 0.1f));
-		pDraw->AddRect(timelineRect.Min - ImVec2(10, 0), ImVec2(timelineRect.Max.x + 10, timelineRect.Min.y + gStyle.BarHeight), ImColor(1.0f, 1.0f, 1.0f, 0.4f));
-		for (int i = 0; i < gStyle.MaxTime; i += 2)
+		pDraw->AddRectFilled(timelineRect.Min, ImVec2(timelineRect.Max.x, timelineRect.Min.y + style.BarHeight), ImColor(0.0f, 0.0f, 0.0f, 0.1f));
+		pDraw->AddRect(timelineRect.Min - ImVec2(10, 0), ImVec2(timelineRect.Max.x + 10, timelineRect.Min.y + style.BarHeight), ImColor(1.0f, 1.0f, 1.0f, 0.4f));
+		for (int i = 0; i < style.MaxTime; i += 2)
 		{
 			float x0 = tickScale * MsToTicks((float)i);
 			float msWidth = tickScale * MsToTicks(1);
 			ImVec2 tickPos = ImVec2(cursor.x + x0, timelineRect.Min.y);
-			pDraw->AddRectFilled(tickPos + ImVec2(0, gStyle.BarHeight), tickPos + ImVec2(msWidth, timelineRect.Max.y), ImColor(1.0f, 1.0f, 1.0f, 0.02f));
+			pDraw->AddRectFilled(tickPos + ImVec2(0, style.BarHeight), tickPos + ImVec2(msWidth, timelineRect.Max.y), ImColor(1.0f, 1.0f, 1.0f, 0.02f));
 			const char* pBarText;
 			ImFormatStringToTempBuffer(&pBarText, nullptr, "%d ms", i);
-			pDraw->AddText(tickPos + ImVec2(5, 0), ImColor(gStyle.BGTextColor), pBarText);
-			pDraw->AddLine(tickPos + ImVec2(0, gStyle.BarHeight * 0.5f), tickPos + ImVec2(0, gStyle.BarHeight), ImColor(gStyle.BGTextColor));
-			pDraw->AddLine(tickPos + ImVec2(msWidth, gStyle.BarHeight * 0.75f), tickPos + ImVec2(msWidth, gStyle.BarHeight), ImColor(gStyle.BGTextColor));
+			pDraw->AddText(tickPos + ImVec2(5, 0), ImColor(style.BGTextColor), pBarText);
+			pDraw->AddLine(tickPos + ImVec2(0, style.BarHeight * 0.5f), tickPos + ImVec2(0, style.BarHeight), ImColor(style.BGTextColor));
+			pDraw->AddLine(tickPos + ImVec2(msWidth, style.BarHeight * 0.75f), tickPos + ImVec2(msWidth, style.BarHeight), ImColor(style.BGTextColor));
 		}
-		cursor.y += gStyle.BarHeight;
+		cursor.y += style.BarHeight;
 
-		ImGui::PushClipRect(timelineRect.Min + ImVec2(0, gStyle.BarHeight), timelineRect.Max, true);
+		ImGui::PushClipRect(timelineRect.Min + ImVec2(0, style.BarHeight), timelineRect.Max, true);
 
 		// Common function to draw a single bar
 		/*
@@ -189,15 +196,15 @@ void DrawProfilerHUD()
 			{
 				float startPos = tickScale * (beginTicks < beginAnchor ? 0 : beginTicks - beginAnchor);
 				float endPos = tickScale * (endTicks - beginAnchor);
-				float y = depth * gStyle.BarHeight;
-				ImRect itemRect = ImRect(cursor + ImVec2(startPos, y), cursor + ImVec2(endPos, y + gStyle.BarHeight));
+				float y = depth * style.BarHeight;
+				ImRect itemRect = ImRect(cursor + ImVec2(startPos, y), cursor + ImVec2(endPos, y + style.BarHeight));
 
 				if (ImGui::ItemAdd(itemRect, id, 0))
 				{
-					ImColor color = barColor * gStyle.BarColorMultiplier;
-					ImColor textColor = gStyle.FGTextColor;
+					ImColor color = barColor * style.BarColorMultiplier;
+					ImColor textColor = style.FGTextColor;
 					// Fade out the bars that don't match the filter
-					if (gHUDContext.SearchString[0] != 0 && !strstr(pName, gHUDContext.SearchString))
+					if (context.SearchString[0] != 0 && !strstr(pName, context.SearchString))
 					{
 						color.Value.w *= 0.3f;
 						textColor.Value.w *= 0.5f;
@@ -221,21 +228,21 @@ void DrawProfilerHUD()
 					{
 						// Zoom ration to make the bar fit the entire window
 						float zoom = timelineWidth / itemRect.GetWidth();
-						gHUDContext.TimelineScale = zoom;
+						context.TimelineScale = zoom;
 
 						// Recompute the timeline size with new zoom
-						float newTimelineWidth = timelineRect.GetWidth() * gHUDContext.TimelineScale;
+						float newTimelineWidth = timelineRect.GetWidth() * context.TimelineScale;
 						float newTickScale = newTimelineWidth / ticksInTimeline;
 						float newStartPos = newTickScale * (beginTicks - beginAnchor);
 
-						gHUDContext.TimelineOffset.x = -newStartPos;
+						context.TimelineOffset.x = -newStartPos;
 					}
 
 					// Draw the bar rect and outline if hovered
-					const ImVec2 padding(gStyle.BarPadding, gStyle.BarPadding);
+					const ImVec2 padding(style.BarPadding, style.BarPadding);
 					pDraw->AddRectFilledMultiColor(itemRect.Min + padding, itemRect.Max - padding, color, color, colorBottom, colorBottom);
 					if (hovered)
-						pDraw->AddRect(itemRect.Min, itemRect.Max, ImColor(gStyle.BarHighlightColor), 0.0f, ImDrawFlags_None, 3.0f);
+						pDraw->AddRect(itemRect.Min, itemRect.Max, ImColor(style.BarHighlightColor), 0.0f, ImDrawFlags_None, 3.0f);
 
 					// If the bar size is large enough, draw the name of the bar on top
 					if (itemRect.GetWidth() > 10.0f)
@@ -249,7 +256,7 @@ void DrawProfilerHUD()
 						float etcWidth = 20.0f;
 						if (textSize.x < itemRect.GetWidth() * 0.9f)
 						{
-							pDraw->AddText(itemRect.Min + (ImVec2(itemRect.GetWidth(), gStyle.BarHeight) - textSize) * 0.5f, textColor, pBarText);
+							pDraw->AddText(itemRect.Min + (ImVec2(itemRect.GetWidth(), style.BarHeight) - textSize) * 0.5f, textColor, pBarText);
 						}
 						else if (itemRect.GetWidth() > etcWidth + 10)
 						{
@@ -265,7 +272,7 @@ void DrawProfilerHUD()
 
 							float textWidth = ImGui::CalcTextSize(pBarText, pChar).x;
 
-							ImVec2 textPos = itemRect.Min + ImVec2(4, (gStyle.BarHeight - textSize.y) * 0.5f);
+							ImVec2 textPos = itemRect.Min + ImVec2(4, (style.BarHeight - textSize.y) * 0.5f);
 							pDraw->AddText(textPos, textColor, pBarText, pChar);
 							pDraw->AddText(textPos + ImVec2(textWidth, 0), textColor, pEtc);
 						}
@@ -280,7 +287,7 @@ void DrawProfilerHUD()
 		*/
 		auto TrackHeader = [&](const char* pName, uint32 id)
 		{
-			pDraw->AddRectFilled(ImVec2(timelineRect.Min.x, cursor.y), ImVec2(timelineRect.Max.x, cursor.y + gStyle.BarHeight), ImColor(0.0f, 0.0f, 0.0f, 0.3f));
+			pDraw->AddRectFilled(ImVec2(timelineRect.Min.x, cursor.y), ImVec2(timelineRect.Max.x, cursor.y + style.BarHeight), ImColor(0.0f, 0.0f, 0.0f, 0.3f));
 
 			bool isOpen = ImGui::GetCurrentWindow()->StateStorage.GetBool(id, true);
 			ImVec2 trackTextCursor = ImVec2(timelineRect.Min.x, cursor.y);
@@ -288,7 +295,7 @@ void DrawProfilerHUD()
 			float caretSize = ImGui::GetTextLineHeight();
 			if (ImGui::ItemAdd(ImRect(trackTextCursor, trackTextCursor + ImVec2(caretSize, caretSize)), id))
 			{
-				pDraw->AddText(ImGui::GetItemRectMin() + ImVec2(2, 2), ImColor(gStyle.BGTextColor), isOpen ? ICON_FA_CARET_DOWN : ICON_FA_CARET_RIGHT);
+				pDraw->AddText(ImGui::GetItemRectMin() + ImVec2(2, 2), ImColor(style.BGTextColor), isOpen ? ICON_FA_CARET_DOWN : ICON_FA_CARET_RIGHT);
 				if (ImGui::ButtonBehavior(ImRect(ImGui::GetItemRectMin(), ImGui::GetItemRectMax()), id, nullptr, nullptr, ImGuiButtonFlags_MouseButtonLeft))
 				{
 					isOpen = !isOpen;
@@ -296,7 +303,7 @@ void DrawProfilerHUD()
 				}
 			}
 			trackTextCursor.x += caretSize;
-			pDraw->AddText(trackTextCursor, ImColor(gStyle.BGTextColor), pName);
+			pDraw->AddText(trackTextCursor, ImColor(style.BGTextColor), pName);
 			return isOpen;
 		};
 
@@ -308,9 +315,9 @@ void DrawProfilerHUD()
 
 			// Add thread name for track
 			bool isOpen = TrackHeader(queue.Name, ImGui::GetID(&queue));
-			uint32 maxDepth = isOpen ? gStyle.MaxDepth : 1;
+			uint32 maxDepth = isOpen ? style.MaxDepth : 1;
 			uint32 trackDepth = 1;
-			cursor.y += gStyle.BarHeight;
+			cursor.y += style.BarHeight;
 
 			// Add a bar in the right place for each sample region
 			/*
@@ -341,12 +348,12 @@ void DrawProfilerHUD()
 				});
 
 			// Add vertical line to end track
-			cursor.y += trackDepth * gStyle.BarHeight;
-			pDraw->AddLine(ImVec2(timelineRect.Min.x, cursor.y), ImVec2(timelineRect.Max.x, cursor.y), ImColor(gStyle.BGTextColor));
+			cursor.y += trackDepth * style.BarHeight;
+			pDraw->AddLine(ImVec2(timelineRect.Min.x, cursor.y), ImVec2(timelineRect.Max.x, cursor.y), ImColor(style.BGTextColor));
 		}
 
 		// Split between GPU and CPU tracks
-		pDraw->AddLine(ImVec2(timelineRect.Min.x, cursor.y), ImVec2(timelineRect.Max.x, cursor.y), ImColor(gStyle.BGTextColor), 4);
+		pDraw->AddLine(ImVec2(timelineRect.Min.x, cursor.y), ImVec2(timelineRect.Max.x, cursor.y), ImColor(style.BGTextColor), 4);
 
 		// Draw each CPU thread track
 		Span<FooProfiler::ThreadData> threads = gProfiler.GetThreads();
@@ -358,9 +365,9 @@ void DrawProfilerHUD()
 			ImFormatStringToTempBuffer(&pHeaderText, nullptr, "%s [%d]", thread.Name, thread.ThreadID);
 			bool isOpen = TrackHeader(pHeaderText, ImGui::GetID(&thread));
 
-			uint32 maxDepth = isOpen ? gStyle.MaxDepth : 1;
+			uint32 maxDepth = isOpen ? style.MaxDepth : 1;
 			uint32 trackDepth = 1;
-			cursor.y += gStyle.BarHeight;
+			cursor.y += style.BarHeight;
 
 			// Add a bar in the right place for each sample region
 			/*
@@ -388,8 +395,8 @@ void DrawProfilerHUD()
 				});
 
 			// Add vertical line to end track
-			cursor.y += trackDepth * gStyle.BarHeight;
-			pDraw->AddLine(ImVec2(timelineRect.Min.x, cursor.y), ImVec2(timelineRect.Max.x, cursor.y), ImColor(gStyle.BGTextColor));
+			cursor.y += trackDepth * style.BarHeight;
+			pDraw->AddLine(ImVec2(timelineRect.Min.x, cursor.y), ImVec2(timelineRect.Max.x, cursor.y), ImColor(style.BGTextColor));
 		}
 
 		// The final height of the timeline
@@ -399,25 +406,25 @@ void DrawProfilerHUD()
 		{
 			// Profile range
 			// If not currently in selection, start selection when left mouse button is pressed
-			if (!gHUDContext.IsSelectingRange && ImGui::IsMouseHoveringRect(timelineRect.Min, timelineRect.Max))
+			if (!context.IsSelectingRange && ImGui::IsMouseHoveringRect(timelineRect.Min, timelineRect.Max))
 			{
 				if (ImGui::IsMouseClicked(ImGuiMouseButton_Left))
 				{
-					gHUDContext.RangeSelectionStart = ImGui::GetMousePos().x;
-					gHUDContext.IsSelectingRange = true;
+					context.RangeSelectionStart = ImGui::GetMousePos().x;
+					context.IsSelectingRange = true;
 				}
 			}
-			else if(gHUDContext.IsSelectingRange)
+			else if (context.IsSelectingRange)
 			{
 				// If mouse button is released, exit measuring mode
 				if (ImGui::IsMouseReleased(ImGuiMouseButton_Left))
 				{
-					gHUDContext.IsSelectingRange = false;
+					context.IsSelectingRange = false;
 				}
 				else
 				{
 					// Distance between mouse cursor and measuring start
-					float distance = fabs(ImGui::GetMousePos().x - gHUDContext.RangeSelectionStart);
+					float distance = fabs(ImGui::GetMousePos().x - context.RangeSelectionStart);
 
 					// Fade in based on distance
 					float opacity = ImClamp(distance / 30.0f, 0.0f, 1.0f);
@@ -426,14 +433,14 @@ void DrawProfilerHUD()
 						float time = TicksToMs(distance / tickScale);
 
 						// Draw measure region
-						pDraw->AddRectFilled(ImVec2(gHUDContext.RangeSelectionStart, timelineRect.Min.y), ImVec2(ImGui::GetMousePos().x, timelineRect.Max.y), ImColor(1.0f, 1.0f, 1.0f, 0.1f));
-						pDraw->AddLine(ImVec2(gHUDContext.RangeSelectionStart, timelineRect.Min.y), ImVec2(gHUDContext.RangeSelectionStart, timelineRect.Max.y), ImColor(1.0f, 1.0f, 1.0f, 0.3f), 3.0f);
+						pDraw->AddRectFilled(ImVec2(context.RangeSelectionStart, timelineRect.Min.y), ImVec2(ImGui::GetMousePos().x, timelineRect.Max.y), ImColor(1.0f, 1.0f, 1.0f, 0.1f));
+						pDraw->AddLine(ImVec2(context.RangeSelectionStart, timelineRect.Min.y), ImVec2(context.RangeSelectionStart, timelineRect.Max.y), ImColor(1.0f, 1.0f, 1.0f, 0.3f), 3.0f);
 						pDraw->AddLine(ImVec2(ImGui::GetMousePos().x, timelineRect.Min.y), ImVec2(ImGui::GetMousePos().x, timelineRect.Max.y), ImColor(1.0f, 1.0f, 1.0f, 0.3f), 3.0f);
 
 						// Add line and arrows
-						ImColor measureColor = gStyle.FGTextColor;
+						ImColor measureColor = style.FGTextColor;
 						measureColor.Value.w *= opacity;
-						ImVec2 lineStart = ImVec2(gHUDContext.RangeSelectionStart, ImGui::GetMousePos().y);
+						ImVec2 lineStart = ImVec2(context.RangeSelectionStart, ImGui::GetMousePos().y);
 						ImVec2 lineEnd = ImGui::GetMousePos();
 						if (lineStart.x > lineEnd.x)
 							std::swap(lineStart.x, lineEnd.x);
@@ -460,14 +467,14 @@ void DrawProfilerHUD()
 			if (zoomDelta != 0)
 			{
 				// Logarithmic scale
-				float logScale = logf(gHUDContext.TimelineScale);
+				float logScale = logf(context.TimelineScale);
 				logScale += zoomDelta;
 				float newScale = ImClamp(expf(logScale), 1.0f, 100.0f);
 
-				float scaleFactor = newScale / gHUDContext.TimelineScale;
-				gHUDContext.TimelineScale *= scaleFactor;
+				float scaleFactor = newScale / context.TimelineScale;
+				context.TimelineScale *= scaleFactor;
 				ImVec2 mousePos = ImGui::GetMousePos() - timelineRect.Min;
-				gHUDContext.TimelineOffset.x = mousePos.x - (mousePos.x - gHUDContext.TimelineOffset.x) * scaleFactor;
+				context.TimelineOffset.x = mousePos.x - (mousePos.x - context.TimelineOffset.x) * scaleFactor;
 			}
 		}
 
@@ -475,17 +482,17 @@ void DrawProfilerHUD()
 		bool held;
 		ImGui::ButtonBehavior(timelineRect, timelineID, nullptr, &held, ImGuiButtonFlags_MouseButtonRight);
 		if (held)
-			gHUDContext.TimelineOffset += ImGui::GetIO().MouseDelta;
+			context.TimelineOffset += ImGui::GetIO().MouseDelta;
 
 		// Compute the new timeline size to correctly clamp the offset
-		timelineWidth = timelineRect.GetWidth() * gHUDContext.TimelineScale;
-		gHUDContext.TimelineOffset = ImClamp(gHUDContext.TimelineOffset, ImMin(ImVec2(0.0f, 0.0f), timelineRect.GetSize() - ImVec2(timelineWidth, timelineHeight)), ImVec2(0.0f, 0.0f));
+		timelineWidth = timelineRect.GetWidth() * context.TimelineScale;
+		context.TimelineOffset = ImClamp(context.TimelineOffset, ImMin(ImVec2(0.0f, 0.0f), timelineRect.GetSize() - ImVec2(timelineWidth, timelineHeight)), ImVec2(0.0f, 0.0f));
 
 		ImGui::PopClipRect();
 		ImGui::PopClipRect();
 
 		// Draw a debug rect around the timeline item and the whole (unclipped) timeline rect
-		if (gStyle.DebugMode)
+		if (style.DebugMode)
 		{
 			pDraw->PushClipRectFullScreen();
 			pDraw->AddRect(cursorStart, cursorStart + ImVec2(timelineWidth, timelineHeight), ImColor(1.0f, 0.0f, 0.0f), 0.0f, ImDrawFlags_None, 3.0f);
@@ -494,13 +501,13 @@ void DrawProfilerHUD()
 		}
 
 		// Horizontal scroll bar
-		ImS64 scrollH = -(ImS64)gHUDContext.TimelineOffset.x;
+		ImS64 scrollH = -(ImS64)context.TimelineOffset.x;
 		ImGui::ScrollbarEx(ImRect(ImGui::GetCursorScreenPos(), ImGui::GetCursorScreenPos() + ImGui::GetContentRegionAvail()), ImGui::GetID("ScrollH"), ImGuiAxis_X, &scrollH, (ImS64)timelineRect.GetSize().x, (ImS64)timelineWidth, ImDrawFlags_None);
-		gHUDContext.TimelineOffset.x = -(float)scrollH;
+		context.TimelineOffset.x = -(float)scrollH;
 
 		// Vertical scroll bar
-		ImS64 scrollV = -(ImS64)gHUDContext.TimelineOffset.y;
+		ImS64 scrollV = -(ImS64)context.TimelineOffset.y;
 		ImGui::ScrollbarEx(ImRect(ImVec2(timelineRect.Max.x, timelineRect.Min.y), ImVec2(timelineRect.Max.x + 15, timelineRect.Max.y)), ImGui::GetID("ScrollV"), ImGuiAxis_Y, &scrollV, (ImS64)timelineRect.GetSize().y, (ImS64)timelineHeight, ImDrawFlags_None);
-		gHUDContext.TimelineOffset.y = -(float)scrollV;
+		context.TimelineOffset.y = -(float)scrollV;
 	}
 }
